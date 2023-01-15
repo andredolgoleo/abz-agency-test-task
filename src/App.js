f291751ece8963a06b14b73fc2f8a9f513f70b27
Andrey Dolhov <andrej.dolgov.042110@gmail.com>
2023-01-15 21:55:31 +0200

    users added

--- src/App.js
+++ src/App.js
@@ -2,16 +2,27 @@ import './App.css';
 import './styles/index.scss';
 import { FirstPage } from "./components/FirtsPage";
 import { Users } from "./components/Users";
+import {useEffect, useState} from "react";
+import {getUsers} from "./api/api";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  console.log(data)
+
+  useEffect(() => {
+    getUsers(1).then(data => setData([...data.users]));
+
+  }, [])
+
   return (
     <div className='container'>
       <FirstPage />
       <main className='main'>
         <h2 className='main__title'>Working with GET request</h2>
 
-        <section>
-          <Users />
+        <section className='main__users'>
+          <Users users={data} />
         </section>
       </main>
     </div>
