e604767f556c59962a739028d49bb7116141c820
Andrey Dolhov <andrej.dolgov.042110@gmail.com>
2023-01-15 11:50:42 +0200

    first page added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import './styles/index.scss';
+import { FirstPage } from "./components/FirtsPage";
+import { Users } from "./components/Users";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <FirstPage />
+      <main className='main'>
+        <h2 className='main__title'>Working with GET request</h2>
+
+        <section>
+          <Users />
+        </section>
+      </main>
     </div>
   );
 }
